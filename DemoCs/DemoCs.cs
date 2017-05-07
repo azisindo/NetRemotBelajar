3eba290f2b5289d5c8b227f06df2fb0a435bec22
azisindo <aziz.mcgr@gmail.com>
2017-05-07 17:39:49 +0700

    baru sampai isi

---
+++ DemoCs/DemoCs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCs
+{
+    class DemoCs
+    {
+        [Serializable]
+        public class InformationBucket
+        {
+            public int Value;
+            public string Message;
+        }
+        public class DemoClas:MarshalByRefObject
+        {
+            private int Value =0;
+            private string Message="";
+
+            public void SetValue(int InputValue)
+            {
+                Value = InputValue;
+                Console.WriteLine("Demo Class : setting Value to {0}", InputValue);
+            }
+            
+
+
+
+        }
+
+
+    }
+}
